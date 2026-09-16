fa9372dc495c61bedfd12bf970c58fcebff7589b
tkitch1983-sudo <tkitch1983@gmail.com>
2026-09-16 19:51:42 +0100

    Match target sheets to workbook calculations

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='neas-shift-board-shell-v7';
+const CACHE='neas-shift-board-shell-v8';
 const SHELL=['./','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();

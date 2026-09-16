a5f3e9570e0c702a6486a6160010956beeac8639
tkitch1983-sudo <tkitch1983@gmail.com>
2026-09-16 19:59:41 +0100

    Add live target sheet sales formulas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='neas-shift-board-shell-v8';
+const CACHE='neas-shift-board-shell-v9';
 const SHELL=['./','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();

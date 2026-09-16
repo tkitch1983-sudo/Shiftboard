f9cb8f116280da80d8153680ea9e132b730a44fb
tkitch1983-sudo <tkitch1983@gmail.com>
2026-09-16 20:17:04 +0100

    Update Lee hours and workshop auto clock-out

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='neas-shift-board-shell-v9';
+const CACHE='neas-shift-board-shell-v10';
 const SHELL=['./','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();

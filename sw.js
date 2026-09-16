1077a7a816bb264ec813434d0d0af1c49d6330e8
tkitch1983-sudo <tkitch1983@gmail.com>
2026-09-16 16:26:26 +0100

    Apply workshop and filling station time rounding

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='neas-shift-board-shell-v4';
+const CACHE='neas-shift-board-shell-v5';
 const SHELL=['./','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();

af006b2e8e1871b5a930071544d91d8b8e1efd41
tkitch1983-sudo <tkitch1983@gmail.com>
2026-09-17 07:08:15 +0100

    Refresh cache for live sales update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='neas-shift-board-shell-v14';
+const CACHE='neas-shift-board-shell-v15';
 const SHELL=['./','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();

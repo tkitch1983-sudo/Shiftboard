b745374b11e225ff4a5bc53024644e232c0181f8
tkitch1983-sudo <tkitch1983@gmail.com>
2026-09-24 15:53:42 +0100

    Deploy clock-out reason options

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE='neas-shift-board-shell-v32';
-const SHELL=['./','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png','./pins-tab.js','./bradford-fix.js','./clock-fix.js','./hartlepool-order.js','./timesheet-print-fix.js'];
+const CACHE='neas-shift-board-shell-v33';
+const SHELL=['./','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png','./pins-tab.js','./bradford-fix.js','./clock-fix.js','./hartlepool-order.js','./timesheet-print-fix.js','./clock-out-options.js'];
 
 async function withPinsTab(response){
   if(!response) return response;
@@ -11,7 +11,8 @@ async function withPinsTab(response){
   const hasClockFix=html.includes('<script src="./clock-fix.js');
   const hasHartlepoolOrder=html.includes('<script src="./hartlepool-order.js');
   const hasTimesheetPrintFix=html.includes('<script src="./timesheet-print-fix.js');
-  if(hasPins && hasBradford && hasClockFix && hasHartlepoolOrder && hasTimesheetPrintFix) return new Response(html,{status:response.status,statusText:response.statusText,headers:response.headers});
+  const hasClockOutOptions=html.includes('<script src="./clock-out-options.js');
+  if(hasPins && hasBradford && hasClockFix && hasHartlepoolOrder && hasTimesheetPrintFix && hasClockOutOptions) return new Response(html,{status:response.status,statusText:response.statusText,headers:response.headers});
   const closeBody=html.toLowerCase().lastIndexOf('</body>');
   if(closeBody<0) return new Response(html,{status:response.status,statusText:response.statusText,headers:response.headers});
   let extra='';
@@ -20,6 +21,7 @@ async function withPinsTab(response){
   if(!hasClockFix) extra+='<script src="./clock-fix.js?v=1"></script>';
   if(!hasHartlepoolOrder) extra+='<script src="./hartlepool-order.js?v=2"></script>';
   if(!hasTimesheetPrintFix) extra+='<script src="./timesheet-print-fix.js?v=2"></script>';
+  if(!hasClockOutOptions) extra+='<script src="./clock-out-options.js?v=1"></script>';
   const patched=html.slice(0,closeBody)+extra+html.slice(closeBody);
   const headers=new Headers(response.headers);
   headers.delete('content-length');

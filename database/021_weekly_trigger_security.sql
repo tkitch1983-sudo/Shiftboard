f6c3fdbdbd4d314b85d6fe477184119e2268d58d
tkitch1983-sudo <tkitch1983@gmail.com>
2026-09-26 06:23:49 +0100

    Harden weekly check trigger function

---
+++ database/021_weekly_trigger_security.sql
@@ -0,0 +1,3 @@
+revoke all on function public.stamp_weekly_site_check() from public;
+revoke all on function public.stamp_weekly_site_check() from anon;
+revoke all on function public.stamp_weekly_site_check() from authenticated;
